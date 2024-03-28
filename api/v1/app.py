6a3e30a3a1595f857a81ec1802a6e2f5d027fc16
duncmv <dtindiwensi@gmail.com>
2024-03-28 21:53:46 +0300

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """api app v1"""
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -8,6 +8,11 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(err):
+    return make_response(jsonify(error="Not found"), 404)
 
 
 @app.teardown_appcontext
