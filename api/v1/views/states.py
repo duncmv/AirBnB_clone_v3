09bbdef089a627c4282e20d411aa068f0d9a7674
duncmv <dtindiwensi@gmail.com>
2024-03-29 13:23:17 +0300

    task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -25,15 +25,30 @@ def state(state_id=None):
         state = storage.get(State, state_id)
         if state is not None:
             storage.delete(state)
+            storage.save()
             return jsonify({})
         abort(404)
 
     if request.method == 'POST':
-        if not request.json:
-            return "Not a JSON", 400
-        if 'name' in request.json:
-            return "Missing name", 400
-        params = request.json
+        params = request.get_json(silent=True)
+        if not params:
+            return make_response("Not a JSON\n", 400)
+        if 'name' not in params:
+            return make_response("Missing name\n", 400)
         new = State(**params)
         new.save()
-        return jsonify(new.to_dict())
+        return jsonify(new.to_dict()), 201
+
+    if request.method == 'PUT':
+        state = storage.get(State, state_id)
+        if state is None:
+            abort(404)
+        params = request.get_json(silent=True)
+        if not params:
+            return make_response("Not a JSON\n", 400)
+        for k in ("id", "created_at", "updated_at"):
+            params.pop(k, None)
+        for k, v in params.items():
+            setattr(state, k, v)
+        state.save()
+        return jsonify(state.to_dict())
