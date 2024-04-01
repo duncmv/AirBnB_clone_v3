50df60dc7dacfbdcae003754eba5d8954d5fb819
duncmv <dtindiwensi@gmail.com>
2024-04-01 10:44:49 +0300

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,23 @@
 """starts a status route"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns the status"""
     return jsonify(status="OK")
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """returns the stats"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
