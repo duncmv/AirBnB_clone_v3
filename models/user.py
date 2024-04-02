549a71d126f96c5d220d163677daa017cc6b6467
duncmv <dtindiwensi@gmail.com>
2024-04-02 11:43:47 +0300

    task 14

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -13,7 +14,7 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column("password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
@@ -26,4 +27,15 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        value = kwargs.get("password", "")
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self.__dict__.get('_password', "")
+
+    @password.setter
+    def password(self, value):
+        """hash the password"""
+        pass_b = bytes(value.encode("utf-8"))
+        self.__dict__['_password'] = hashlib.md5(pass_b).hexdigest()
